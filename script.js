b13a90432eed8b8ba4f746ba92165812d2a0cb30
sannidhya verma <sannidhyaverma1@gmail.com>
2026-02-24 15:26:59 +0530

    1

---
+++ script.js
@@ -0,0 +1,60 @@
+// console.log("abes");
+// var a=10;
+// var b=20;
+// var c=a+b;
+// console.log(c);
+// let d=30;
+// d=50;
+// const e=40;
+// console.log(d);
+// console.log(e);
+
+// let person={
+//     name:"John",
+//     age:30, }
+
+// console.log(person.name);
+// console.log(person.age);
+// let a=20;
+// let b=30;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b); 
+// let temperature=25;
+// if(temperature>30){
+//     console.log("It's a hot day");
+// }else if(temperature<15){
+//     console.log("It's a cold day");
+// }
+// else{
+//     console.log("It's a nice day");
+// }   
+
+// let day="Wednesday";
+// switch(day){
+//     case "Monday":  
+//     console.log("It's Monday");
+//     break;
+//     case "Tuesday":
+//     console.log("It's Tuesday");
+//     break;  
+//     case "Wednesday":
+//     console.log("It's Wednesday");
+//     break;
+//     case "Thursday":        
+//     console.log("It's Thursday");
+//     break;
+//     case "Friday":
+//     console.log("It's Friday");
+//     break;
+//     case "Saturday":
+//     console.log("It's Saturday");
+//     break;
+//     case "Sunday":
+//     console.log("It's Sunday");     
+//     break;
+//     default:
+//     console.log("Invalid day");
+// }
