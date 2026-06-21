23050634d64670ffdfcfddc8d42b66b853bc423d
sannidhya <sannidhyaverma1@gmail.com>
2026-06-22 02:19:30 +0530

    Add files via upload

---
+++ main7.jsx
@@ -0,0 +1,1 @@
+ import React from "react"; import ReactDOM from "react-dom/client"; import App from "backend-sem2/app.jsx"; import "backend-sem2/app.jsx"; ReactDOM.createRoot(document.getElementById("root")).render( <React.StrictMode> <App /> </React.StrictMode> );  
